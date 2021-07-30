dee703f6a13991a81c6700ceb777308b88187a3d
원종빈의 MacBook <gghkdu2@gmail.com>
2021-07-30 10:22:24 +0900

    mongo db connect

---
+++ src/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose"
+
+mongoose.connect("mongodb://127.0.0.1:27017/wetube", { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+});   // 주소 맨뒤에 db 이름을 명시
+
+
+const db = mongoose.connection
+
+const handleOpen = () => console.log("connected to DB ✅");
+const handleError = (error) => console.log("❌ DB Error ❌ ", error);
+
+db.on("error", handleError);
+db.once("open", handleOpen);
+
