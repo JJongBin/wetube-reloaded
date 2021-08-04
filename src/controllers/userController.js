94c9c06a9c27523dd418f832af57a7d8a627f5b7
원종빈의 MacBook <gghkdu2@gmail.com>
2021-08-04 15:49:36 +0900

    login password 비교 기능 추가

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async(req, res) => {
@@ -17,18 +18,50 @@ export const postJoin = async(req, res) => {
     if (usernameExists) {
         return res.status(400).render("join", {pageTitle, errorMessage: "This username/email is already taken."})
     }
+    
+    try{
+        await User.create({
+            name, 
+            username, 
+            email, 
+            password, 
+            location,
+        }) 
+        res.redirect("/login");
+      }
+      catch(error){
+        return res.status(400).render("join", {pageTitle:"Upload Video", errorMessage: error._message,})
+      }
 
-    await User.create({
-        name, 
-        username, 
-        email, 
-        password, 
-        location,
-    }) 
-    res.redirect("/login");
+
 }
 
-export const login = (req, res) => res.send("Login");
+
+export const getLogin = (req, res) => {
+    res.render("login", {pageTitle:"Login", })
+}
+
+export const postLogin = async(req, res) => {
+    const { username, password } = req.body;
+    const pageTitle = "Login"
+    const user = await User.findOne({ username });
+
+    if(!user){
+        res.status(400).render("login", {pageTitle, errorMessage:"An account with this username does not exists."})
+        
+    }
+    console.log(user.password)
+    const ok = await bcrypt.compare(password, user.password);
+    if(!ok){
+        res.status(400).render("login", {
+            pageTitle, 
+            errorMessage:"Wrong password",
+        })
+    }
+    console.log("LOG USER IN! COMING SOON!")
+    return res.redirect("/")
+}
+
 
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Delete User");
