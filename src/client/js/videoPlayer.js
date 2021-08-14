f07056ed0a301a6243d9ccc35163262c481d8f47
원종빈의 MacBook <gghkdu2@gmail.com>
2021-08-14 11:18:20 +0900

    전체시간, 진행시간, 시간 포맷팅

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -4,6 +4,10 @@ const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
+
+
 
 let volumeValue = 0.5
 video.volume = volumeValue;
@@ -43,8 +47,27 @@ const handleVolumeChange = (event) => {
 }
 
 
+const formatTime = (seconds) => new Date(seconds*1000).toISOString().substr(11, 8);
+
+const handleLoadedMetadata = () => {
+    totalTime.innerText = formatTime(Math.floor(video.duration));
+
+
+}
+
+const handleTimeUpdate = () => {
+    currentTime.innerText = formatTime(Math.floor(video.currentTime));
+
+}
+
+
+
 
 playBtn.addEventListener("click", handlePlayClick);
 
 muteBtn.addEventListener("click", handleMute);;
 volumeRange.addEventListener("input", handleVolumeChange);
+video.addEventListener("loadedmetadata", handleLoadedMetadata);
+video.addEventListener("timeupdate", handleTimeUpdate);
+
+
