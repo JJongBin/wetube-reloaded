90af4ce84c5da371d72558f0c95b5b9a455f14a0
원종빈의 MacBook <gghkdu2@gmail.com>
2021-08-19 07:22:49 +0900

    video download transcode(mp4), thumbnail

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,4 +1,5 @@
-const { Body } = require("node-fetch");
+// const { Body } = require("node-fetch");
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
 
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
@@ -25,13 +26,14 @@ const handleStart = () => {
     startBtn.removeEventListener("click", handleStart);
     startBtn.addEventListener("click", handleStop);
 
-    recorder = new MediaRecorder(stream)
+    recorder = new MediaRecorder(stream, {mimeType: "video/webm"})
     
     recorder.ondataavailable = (event) => {
         videoFile = URL.createObjectURL(event.data)
+        console.log(videoFile);
         video.srcObject = null;
         video.src = videoFile;
-        video,loop =true;
+        video.loop =true;
         video.play();
     }
     recorder.start();
@@ -45,12 +47,37 @@ const handleStop = () => {
     recorder.stop();
 }
 
-const handleDownload = () => {
+const handleDownload = async () => {
+    const ffmpeg = createFFmpeg({ log: true });
+    await ffmpeg.load();
+
+    ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile));
+    
+    await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4");     // "-r", "60" -> 초당 60 프레임으로
+    
+    await ffmpeg.run("-i", "recording.webm", "-ss", "00:00:01", "-frames:v", "1", "thumbnail.jpg");     // "-frames:v", "1" -> 첫 프레임(한컷)의 스크릿샷을 찍음
+
+
+    const mp4File = ffmpeg.FS("readFile", "output.mp4");
+    const thimbFile = ffmpeg.FS("readFile", "thumbnail.jpg");
+
+    const mp4Blob = new Blob([mp4File.buffer], {type: "video/mp4"});        // blob url은 url을 통해서 파일에 접근
+    const thumbBlob = new Blob([thimbFile.buffer], {type: "image/jpg"});
+
+    const mp4Url = URL.createObjectURL(mp4Blob);
+    const thumbUrl = URL.createObjectURL(thumbBlob);
+
     const a = document.createElement("a");
-    a.href = videoFile;
-    a.download = "MyRecording.webm";
+    a.href = mp4Url;
+    a.download = "MyRecording.mp4";
     document.body.appendChild(a);
     a.click();
+    
+    const thumbA = document.createElement("a");
+    thumbA.href = thumbUrl;
+    thumbA.download = "MyThumbnail.jpg";
+    document.body.appendChild(thumbA);
+    thumbA.click();
 }
 
 
